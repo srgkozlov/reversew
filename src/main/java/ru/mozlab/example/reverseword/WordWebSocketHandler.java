7f1e4900785623e2d80e7e5633b1ad3d0b28b5bf
srgkozlov <mail2serg.kozlov@gmail.com>
2018-06-09 20:15:08 +0300

    +WordService

--- src/main/java/ru/mozlab/example/reverseword/WordWebSocketHandler.java
+++ src/main/java/ru/mozlab/example/reverseword/WordWebSocketHandler.java
@@ -2,6 +2,8 @@ package ru.mozlab.example.reverseword;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.reactive.socket.WebSocketHandler;
@@ -10,14 +12,12 @@ import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 import ru.mozlab.example.reverseword.repository.WordRepository;
 
-@Component
-public class WordWebSocketHandler implements WebSocketHandler {
-
+@Service
+class WordService {
     private static final ObjectMapper om = new ObjectMapper();
+    @Autowired  WordRepository repo;
 
-    @Autowired	WordRepository repo;
-
-    private final String words2json() {
+    public String findAllJSON() {
         String jsonOut = "";
         try {
              jsonOut = om.writeValueAsString(repo.findAll());
@@ -25,18 +25,24 @@ public class WordWebSocketHandler implements WebSocketHandler {
          return jsonOut;
     }
 
-    private final String save2repo(String w) {
-        if (!w.isEmpty()) repo.save(w);
+    public String save(String w) {
+        if (!w.isEmpty()) repo.save(new StringBuilder(w).reverse().toString());
         return w;
     }
+}
+
+@Component
+public class WordWebSocketHandler implements WebSocketHandler {
+    
+    @Autowired
+    WordService service;
 
     @Override
     public Mono<Void> handle(WebSocketSession sess) {
         return sess.send(sess.receive()
             .map(WebSocketMessage::getPayloadAsText)
-            .map(w->new StringBuilder(w).reverse().toString())
-            .map(w->save2repo(w))
-            .map(w->words2json())
+            .map(w->service.save(w))
+            .map(w->service.findAllJSON())
             .map(sess::textMessage));
     }
 }
